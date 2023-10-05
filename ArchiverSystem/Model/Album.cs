230473f1c78c5b5a12c26b0867b8517701774983
amp <amp@thinx.gr>
2023-10-05 11:25:47 +0300

    5/10

---
+++ ArchiverSystem/Model/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiverSystem.Model
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime InputDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+        public Album() 
+        {
+            InputDate = DateTime.Now;
+            UpdateDate = DateTime.Now;
+        }
+
+        public void UpdateAlbum()
+        {
+            UpdateDate = DateTime.Now;
+        }
+
+
+    }
+}
---
+++ ArchiverSystem/Model/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ArchiverSystem.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Qty { get; set; }
+        public Image image {get; set;}
+        public DateTime InputDate { get; set; }
+        public DateTime UpdateDate { get; set;}
+
+        public Item() 
+        {
+            InputDate = DateTime.Now;
+            UpdateDate = DateTime.Now;
+        }
+
+        public void UpdateItem()
+        {
+            UpdateDate = DateTime.Now;
+        }
+
+    }
+}
